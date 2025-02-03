fca201ebd41510a328437b43a7928a578fe3942e
Evgeniy-makdak <makdak23@mail.ru>
2025-02-03 22:56:55 +0300

    Передал динамические параметры из data.js в компоненты OurDesription пропсами.

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
 import Header from './components/header/Header';
+import { ways } from './data'
+import OurDesription from './components/OurDesription';
 import './index.css';
 
 function App() {
@@ -6,6 +8,16 @@ function App() {
     <div>
       <Header />
       <h1 className="mainTitle">Системы водоочистки</h1>
+      <section>
+        <ul>
+          {/* <OurDesription title={ways[0].title} description={ways[0].description} /> Полная запись, которую можно переписать, используя
+          спред оператор ... */}
+          <OurDesription {...ways[0]} />
+          <OurDesription {...ways[1]} />
+          <OurDesription {...ways[2]} />
+          <OurDesription {...ways[3]} />
+        </ul>
+      </section>
     </div>
   );
 }
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,10 +1,12 @@
+import logo from '/public/images/logo.jpg'
+
 export default function Header() {
     const currentTime = new Date().toLocaleTimeString('ru-RU', { hour12: false });
     
     return (
         <header>
             <div className="header-container">
-                <img src="/public/images/logo.jpg" alt="logo" className="logo" />
+                <img src={logo} alt="logo" className="logo" />
                 <span className="time">Текущее время: {currentTime}</span>
             </div>
         </header>
