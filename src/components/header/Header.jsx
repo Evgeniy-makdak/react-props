94524acb3d1cab3f6c13378b3e66016928b71932
Evgeniy-makdak <makdak23@mail.ru>
2025-02-03 10:07:26 +0300

    Добавил таймер (пока статический, обновляется при перерендеринге страницы)

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,10 +1,12 @@
 export default function Header() {
+    const currentTime = new Date().toLocaleTimeString('ru-RU', { hour12: false });
+    
     return (
         <header>
-        <div className="header-container">
-            <img src="/public/images/logo.jpg" alt="logo" className="logo" />
-            <span className="time">Текущее время: </span>
-        </div>
+            <div className="header-container">
+                <img src="/public/images/logo.jpg" alt="logo" className="logo" />
+                <span className="time">Текущее время: {currentTime}</span>
+            </div>
         </header>
     );
 }
